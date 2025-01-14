de9f264d79853d5db521aa636ee2627ca6deedbf
Morg92b <morgan.bouaziz@outlook.fr>
2025-01-14 16:54:04 +0100

    i add login user

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
+const { confirmEmail } = require("./email.controllers");
 
 module.exports.register = async (req, res) => {
     try {
@@ -56,5 +57,34 @@ module.exports.register = async (req, res) => {
         res.status(201).json({ message: "Utilisateur créé ! Veuillez vérifier votre email pour le confirmer." });
     } catch (error) {
         res.status(500).json({ message: "Erreur lors de l'inscription", error: error.message });
+    }
+};
+
+module.exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "Utilisateur non trouvé." });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Mot de passe incorrect." });
+        }
+
+        if (!user.isEmailconfirmed)
+            return res.status(400).json({ message: "Veuilliez confirmer votre e-mail" })
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.status(200).json({ message: "Connexion réussie", token });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur lors de la connexion", error: error.message });
     }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register } = require("../controllers/user.controllers");
+const { register, login } = require("../controllers/user.controllers");
 const { confirmEmail } = require("../controllers/email.controllers");
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
 // Routes Open
 router.post("/register", register);
 router.get("/confirm-email", confirmEmail);
+router.post("/login", login);
 
 
 
