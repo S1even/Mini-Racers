27fb410b2b1335a1371b1d3c18f98c2d15dda682
Bruqui <dylan2003gd@gmail.com>
2025-03-03 22:54:05 +0100

    The page base

---
+++ frontend/src/views/Settings.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home-page">
+        <Navbar />
+    </div>
+    <div>
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import Navbar from '@/components/navbar.vue';
+import Footer from '@/components/footer.vue';
+</script>
+
+<style scoped>
+.home-page {
+  height: 100vh;
+  background-image: url('@/assets/Designer2.png');
+  background-size: cover;
+  background-position: center;
+  background-color: #ffff;
+}
+</style>
