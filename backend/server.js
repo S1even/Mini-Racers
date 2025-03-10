eb4333712f241cb1590586d5522eff65293837e7
Bruqui <dylan2003gd@gmail.com>
2025-03-10 14:45:55 +0100

    add route for confirm email

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/user.routes");
 
 const port = 5500;
 const app = express();
@@ -11,14 +12,14 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
 
 // NAMESPACES
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 if (require.main === module) {
     app.listen(port, () => console.log("Server started at port " + port));
