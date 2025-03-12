72ed25cda5a7037675967ab652e70c49432006ed
Bruqui <dylan2003gd@gmail.com>
2025-03-12 14:25:57 +0100

    Add description for the car of class a

--- frontend/src/views/ClassA.vue
+++ frontend/src/views/ClassA.vue
@@ -31,27 +31,27 @@ const ClassACars = ref([
     {
         name: "Arion",
         image: Arion,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        description: "Built for speed, the Arion is a racing beast with aggressive looks and razor-sharp aerodynamics. With its overpowering engine and XXL spoiler, it devours corners and propels its driver to victory. Its blistering acceleration makes it a formidable weapon on technical circuits, while its exceptional grip enables controlled drifts at high speed.",
     },
     {
         name: "Cerbero",
         image: Cerbero,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        description: "An indomitable beast, built to dominate the asphalt. The Cerbero is a hypercar with a stealthy design and blade-sharp aerodynamics. Its supercharged engine unleashes brutal power, while its ultra-precise handling lets you negotiate every bend at breakneck speed.",
     },
     {
         name: "Mirach",
         image: Mirach,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        description: "The Mirach is a pure marvel of engineering, designed for the most demanding circuits. With its bold aerodynamic design and vibrant red color, it's sure to catch the eye. Its oversized rear wing ensures exceptional grip, even in the tightest corners, while its super-powered engine enables it to reach dizzying speeds in no time.",
     },
     {
         name: "Pegaso",
         image: Pegaso,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        description: "The Pegaso is a concentrate of technology and speed, designed to dominate high-speed circuits. Inspired by Formula 1 single-seaters, it offers an intense and exhilarating racing experience. Its aerodynamic design, streamlined front spoiler and imposing rear spoiler give it exceptional grip and rock-solid stability. Its super-powerful engine enables it to reach stratospheric speeds in a flash.",
     },
     {
         name: "Phaethon",
         image: Phaethon,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        description: "Phaethon embodies raw power and dark elegance. Its name, inspired by the son of Helios who defied the gods, reflects its indomitable temperament. With its violet metallic bodywork, aggressive lines and imposing spoiler, it doesn't go unnoticed. Its roaring engine, like a rumbling storm, announces its devastating power. The Phaethon is designed for drivers who fear nothing and want to dominate the track with implacable force.",
     },
 ]);
 </script>
