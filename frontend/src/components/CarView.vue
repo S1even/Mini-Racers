e2e6cf7142ec347f336d6690ace9d837500e2f7a
Bruqui <dylan2003gd@gmail.com>
2025-03-12 13:49:38 +0100

    Add components for mores details cars

---
+++ frontend/src/components/CarView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="CarsSection">
+        <CarSection
+            v-for="(car, index) in cars"
+            :key="index"
+            :name="car.name"
+            :description="car.description"
+            :image="car.image"
+            :isReversed="index % 2 !== 0"
+            :backgroundColor="backgroundColor"
+            :reverseBackgroundColor="reverseBackgroundColor"
+        />
+    </div>
+</template>
+
+<script setup>
+import CarSection from "@/components/CarSection.vue";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    cars: Array,
+    backgroundColor: String,
+    reverseBackgroundColor: String,
+});
+</script>
+
+<style scoped>
+.CarsSection {
+    font-family: Arial, sans-serif;
+    text-align: center;
+}
+</style>
