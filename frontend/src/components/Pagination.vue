2cfeeb77a3b72775c83af01fb5da3d92abc89524
Bruqui <dylan2003gd@gmail.com>
2025-03-12 13:50:19 +0100

    add components for switch page

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-pag">
+        <a :href="prevLink" class="button-pag" v-if="prevLink">Previous</a>
+        <a :href="nextLink" class="button-pag" v-if="nextLink">Next</a>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    prevLink: String,
+    nextLink: String,
+})
+</script>
+
+<style scoped>
+.container-pag {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(100% - 40px);
+  padding: 20px;
+  margin: 0 auto;
+}
+
+.button-pag {
+  padding: 10px 20px;
+  font-size: 18px;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  background-color: #d67d91;
+  color: white;
+  text-decoration: none;
+  text-align: center;
+}
+</style>
