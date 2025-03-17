c53650626b11e7d5547b5c0faf7d1b994db343a6
Morg92b <morgan.bouaziz@outlook.fr>
2025-03-17 18:18:46 +0100

    try fix cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://mini-racers-git-main-s1evens-projects.vercel.app',
     credentials: true,
 }));
 
