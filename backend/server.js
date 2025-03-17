8eac52c5c08ee452df7ad23f07418403d01ba726
Morg92b <morgan.bouaziz@outlook.fr>
2025-03-17 18:30:07 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://mini-racers-git-main-s1evens-projects.vercel.app'],
     credentials: true,
 }));
 
